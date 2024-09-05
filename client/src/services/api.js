cc9588be33ba0e4527dd850d82e5da80ef5c5f6a
shahar-shemesh <shahar@usa.com>
2024-09-05 19:36:38 +0300

    enable cors

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
 /* ============================================================ */
 export const fetchWeatherData = async (cityName) => {
 
     try {
-        const response = await axios.get(`/api/weather`, {
+        const response = await axios.get(`${API_BASE_URL}/weather`, {
             params: { city: cityName },
         });
         return response.data;
@@ -19,7 +21,7 @@ export const fetchWeatherData = async (cityName) => {
 export const fetchCitySuggestions = async (query) => {
 
     try {
-        const response = await axios.get(`/api/cities`, {
+        const response = await axios.get(`${API_BASE_URL}/cities`, {
             params: { query },
         });
         return response.data;
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,7 @@ module.exports = (app) => {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'https://fintek-weather-app.onrender.com',
-            // target: process.env.REACT_APP_API_BASE_URL,
+            target: process.env.REACT_APP_API_BASE_URL,
             // target: `http://localhost:4000`,
             changeOrigin: true,
         })
