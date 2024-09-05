d985e5a1afe2ebbf3962122b2e09d791abaa191e
shahar-shemesh <shahar@usa.com>
2024-09-05 18:56:28 +0300

    client proxy edited

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = (app) => {
     app.use(
         '/api',
         createProxyMiddleware({
-            // target: process.env.REACT_APP_API_BASE_URL,
-            target: `http://localhost:4000`,
+            target: process.env.REACT_APP_API_BASE_URL,
+            // target: `http://localhost:4000`,
             changeOrigin: true,
         })
     );
