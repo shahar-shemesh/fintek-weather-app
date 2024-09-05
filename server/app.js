066b27d96f28bf982667862dc95a73667f8944dc
shahar-shemesh <shahar@usa.com>
2024-09-05 19:29:21 +0300

    enable cors

--- server/app.js
+++ server/app.js
@@ -8,6 +8,17 @@ const citiesRoutes = require('./routes/citiesAutocomplete');
 
 const app = express();
 app.use(cors());
+
+// enable corse for all origins
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Expose-Headers", "x-total-count");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+    res.header("Access-Control-Allow-Headers", "Content-Type,authorization");
+
+    next();
+});
+
 app.use(express.json());
 app.set('trust proxy', true);
 
